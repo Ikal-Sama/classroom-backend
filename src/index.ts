21d27a4c2e882c0dac6eaeb66c83c22fab2347cf
unknown <joestarjosuke28@gmail.com>
2026-03-07 11:56:04 +0800

    fix deployment2

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,11 @@ import { auth } from './lib/auth';
 const app = express();
 const PORT = 8000;
 
-if (!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not defined in .env file');
 if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL is not defined in .env file');
 }
+if (!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not defined in .env file');
+
 
 // Middleware
 app.use(cors({
