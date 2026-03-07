811a0818314ab986ae176acc5cc4ed6627f6b815
unknown <joestarjosuke28@gmail.com>
2026-03-07 11:53:28 +0800

    fix deployment

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,9 +2,6 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not defined');
-}
+const databaseUrl = process.env.DATABASE_URL;
 
-const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle(sql);
+export const db = drizzle(neon(databaseUrl!));
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ const app = express();
 const PORT = 8000;
 
 if (!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not defined in .env file');
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined in .env file');
+}
 
 // Middleware
 app.use(cors({
